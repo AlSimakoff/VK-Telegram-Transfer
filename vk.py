7de3b5523bce89491b589e8119308a94c85f38be
AlSimakoff <95354645+AlSimakoff@users.noreply.github.com>
2022-08-24 11:29:40 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -1,3 +1,4 @@
+''' pars message from vk '''
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api import VkApi
@@ -6,8 +7,8 @@ import json, random
 import teleg_in
 
 API_VERSION = "5.131"
-GROUP_ID = "164315210"
-GROUP_TOKEN = "vk1.a.IY2abUF4QT-hc3aBXntG1idd-kB2c_SCPOOmwTZA5Xd6NaRDt-V2m8tWQQMKb5VuPRV9WkHoj2PjE7VnIIScZQ52l5QeSauHmqNtKFbe4T1QmfQHA05qyJfgI2pdY9dBycHc3b9RJdPtYuH_sT8eENnRPbEVEZm_g5-1A4xI2RgT9k4vF34xfDCrF61NKpHi"
+GROUP_ID = "***"
+GROUP_TOKEN = "***"
 vk_session = vk_api.VkApi(token=GROUP_TOKEN)
 vk = vk_session.get_api()
 peer_id = "2000000003"
